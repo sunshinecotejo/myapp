eff9849da91c92afb6984b03208e27f9c54e0931
sunshinecotejo <sunshine.cotejo@gmail.com>
2025-05-17 16:32:46 -0400

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
+  def index
+    @products = Product.all
+  end
+  def show
+    @product = Product.find(params[:id])
+  end
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.present?
+      @product.destroy
+    end
+    redirect_to products_path
+  end
+
+private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+     # Only allow a list of trusted parameters through.
+    def product_params
+      params.expect(product: [ :name  ])
+    end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+
+    has_one_attached :featured_image
+    has_rich_text :description
+    
+    validates :name, presence: true
+end
